78c3e17d47ea696c51534fd95be5f6c54504b9c9
querrieoh <67846733+querrieoh@users.noreply.github.com>
2025-01-05 21:16:06 -0500

    Update styleLoader.js

--- src/styleLoader.js
+++ src/styleLoader.js
@@ -1,36 +1,77 @@
 /**
- * Load custom styles from configs.
- * @param {Object} styles - Custom styles.
- * @returns {void}
+ * @class EventEmitter
+ * @property {Map<string, Function[]>} events
+ * @method on - Add a listener to an event
+ * @method emit - Emit an event
+ * @method off - Remove a listener from an event
+ * @method once - Add a listener to an event that will only be called once
  */
-export function loadStyles(styles) {
-	/** @type {HTMLElement} */
-	const root = document.querySelector(":root");
-	for (let [key, val] of Object.entries(styles)) {
-		if (key.includes("FontFamily")) {
-			loadGoogleFont(val);
+class EventEmitter {
+	constructor() {
+		/**
+		 * @type {Map<string, function[]>}
+		 */
+		this.events = new Map();
+	}
+
+	/**
+	 * Add a listener to an event
+	 * @param {string} eventName - The name of the event
+	 * @param {Function} listener - The listener to add
+	 * @returns {void}
+	 */
+	on(eventName, listener) {
+		if (!this.events.has(eventName)) {
+			this.events.set(eventName, []);
 		}
-		root.style.setProperty(convertToCSSVar(key), val);
+		this.events.get(eventName).push(listener);
+	}
+
+	/**
+	 * Emit an event
+	 * @param {string} eventName - The name of the event
+	 * @param  {...any} args - The arguments to pass to the listeners
+	 * @returns {void}
+	 */
+	emit(eventName, ...args) {
+		if (this.events.has(eventName)) {
+			this.events.get(eventName).forEach((listener) => listener(...args));
+		}
+	}
+
+	/**
+	 * Remove a listener from an event
+	 * @param {string} eventName - The name of the event
+	 * @param {Function} listener - The listener to remove
+	 * @returns {void}
+	 */
+	off(eventName, listener) {
+		if (this.events.has(eventName)) {
+			const listeners = this.events.get(eventName);
+			const index = listeners.indexOf(listener);
+			if (index !== -1) {
+				listeners.splice(index, 1);
+				// Clean up memory if there's no listener left
+				if (listeners.length === 0) {
+					this.events.delete(eventName);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Add a listener to an event that will only be called once
+	 * @param {string} eventName - The name of the event
+	 * @param {Function} listener - The listener to add
+	 * @returns {void}
+	 */
+	once(eventName, listener) {
+		const onceWrapper = (...args) => {
+			listener(...args);
+			this.off(eventName, onceWrapper);
+		};
+		this.on(eventName, onceWrapper);
 	}
 }
 
-/**
- * @param {string} font - Font family name.
- * @returns {void}
- */
-function loadGoogleFont(font) {
-	window.WebFont.load({
-		google: {
-			families: [`${font}:100,400,700`],
-		},
-	});
-}
-
-/**
- * @param {string} name - The name of the CSS variable.
- * @returns {string}
- */
-function convertToCSSVar(name) {
-	let cssVar = name.replace(/([A-Z])/g, "-$1").toLowerCase();
-	return `--${cssVar}`;
-}
+export default EventEmitter;
